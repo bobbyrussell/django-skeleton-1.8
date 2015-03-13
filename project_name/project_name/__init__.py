9e5a67a8abcf3816302316f4517e7499d3ecae5a
Bobby Russell <bobbyrussell@gmail.com>
2015-03-13 01:08:28 -0400

    Initial Commit
    Initial Commit of Django Skeleton 1.7 Template Project

---
+++ project_name/project_name/__init__.py
@@ -0,0 +1,7 @@
+"""
+{{ project_name|title }} Project Module
+
+Interesting Docstring goes here!
+"""
+
+__version__ = '0.0.1'
---
+++ project_name/project_name/settings/base.py
@@ -0,0 +1,118 @@
+"""
+Django settings for {{ project_name|title }} Project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+import os
+import sys
+
+DJANGO_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+SITE_ROOT = os.path.dirname(DJANGO_ROOT)
+
+SITE_NAME = os.path.basename(DJANGO_ROOT)
+
+sys.path.append(DJANGO_ROOT)
+
+
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+SECRET_KEY = r"{{ secret_key }}"
+
+DEBUG = True
+
+
+TEMPLATE_DIRS = (os.path.join(DJANGO_ROOT, "templates"),)
+
+TEMPLATE_DEBUG = True
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = '{{ project_name }}.urls'
+
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DJANGO_ROOT, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(DJANGO_ROOT, 'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+FIXTURE_DIRS = (
+    os.path.join(SITE_ROOT, 'fixtures'),
+)
