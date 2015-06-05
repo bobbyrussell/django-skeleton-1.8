c290e506af4cecc1abe73738ff71efb52572a00a
Bobby <bobbyrussell@gmail.com>
2015-06-05 11:02:19 -0400

    Fixed Unreferenced Variable
    Fixed an issue where BASE_DIR was being referenced in
    settings.production by replacing BASE_DIR with DJANGO_ROOT

--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -26,6 +26,6 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DJANGO_ROOT, 'db.sqlite3'),
     }
 }
